376dce50e344ee89c2e1817e4508901c5deaf365
kacpermatrix55 <kacpermatrix55@gmail.com>
2024-10-10 17:21:43 +0200

    nowy komit na masterze

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("zmieniony kod");
         System.out.println("123");
+        System.out.println("dskfj");
+        System.out.println("dsfk");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
